b7f21e41331ef0a1bf71ef3f782a6434e9105a53
fuser <sergioluke35@gmail.com>
2020-05-18 20:53:04 -0300

    fix merge

---
+++ src/main/java/ar/edu/unq/circuito/service/BandaService.java
@@ -0,0 +1,25 @@
+package ar.edu.unq.circuito.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import ar.edu.unq.circuito.model.Banda;
+import ar.edu.unq.circuito.repo.BandaRepository;
+
+@Service
+public class BandaService {
+
+    @Autowired
+    private BandaRepository bandaRepository;
+
+    public Banda guardar(Banda banda) {
+        return bandaRepository.save(banda);
+    }
+
+    public List<Banda> buscarTodos() {
+        return bandaRepository.findAll();
+    }
+
+}
