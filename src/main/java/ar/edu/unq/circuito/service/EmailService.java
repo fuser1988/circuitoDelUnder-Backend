b43b82e56a0dfc88356d9d7fc93270967ac66466
Cristian <cris.esroj@gmail.com>
2020-05-19 16:52:33 -0300

    implemento config para send email

--- src/main/java/ar/edu/unq/circuito/service/EmailService.java
+++ src/main/java/ar/edu/unq/circuito/service/EmailService.java
@@ -1,15 +1,30 @@
 package ar.edu.unq.circuito.service;
 
 import ar.edu.unq.circuito.model.Usuario;
+
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Transactional
 @Service
 public class EmailService {
+	
+	private JavaMailSender javaMailSender;
+
+    public EmailService(JavaMailSender javaMailSender) {
+        this.javaMailSender = javaMailSender;
+    }
 
     void enviarCodigoDeValidacion(String codigo, Usuario usuario) {
-        System.out.println(codigo);
+    	SimpleMailMessage mailMessage = new SimpleMailMessage();
+
+        mailMessage.setTo(usuario.getEmail());
+        mailMessage.setSubject("Codigo De Validación");
+        mailMessage.setText("El Codigo de validación es: " + codigo);
+
+        javaMailSender.send(mailMessage);
     }
-    
+
 }
