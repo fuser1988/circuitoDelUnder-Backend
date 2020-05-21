9d17da7f6bb7e1ed6a490fc5706ccd8e5908f9a9
fuser <sergioluke35@gmail.com>
2020-05-21 14:33:39 -0300

    agrego implementacion de envio de mail

--- src/main/java/ar/edu/unq/circuito/service/EmailService.java
+++ src/main/java/ar/edu/unq/circuito/service/EmailService.java
@@ -1,15 +1,51 @@
 package ar.edu.unq.circuito.service;
 
 import ar.edu.unq.circuito.model.Usuario;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Transactional
 @Service
 public class EmailService {
+    
+    private final JavaMailSender javaMailSender;
+
+    @Autowired
+    public EmailService(JavaMailSender javaMailSender) {
+        this.javaMailSender = javaMailSender;
+    }
 
     void enviarCodigoDeValidacion(String codigo, Usuario usuario) {
-        System.out.println(codigo);
+        MimeMessage mailMessage = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper;
+        try {
+            helper = new MimeMessageHelper(mailMessage, true);
+            helper.setTo(usuario.getEmail());
+            helper.setSubject("Código De Validación");
+            helper.setText(
+                    "<html>"
+                    + "<body>"
+                    + "<div>"
+                    + "<div> Bienvenido" + usuario.getNombre()+ "</div>"
+                    + "<div>Tu código de validación es:</div>"
+                    + "<div><strong>" + codigo + "</strong></div>"
+                    + "<div>Gracias</div>"
+                    + "Circuito del Under"
+                    + "<img src= 'https://raw.githubusercontent.com/fuser1988/circuitoDelUnder-Documentacion/master/Circuito-del-under.png'/>"
+                    + "</div></body>"
+                    + "</html>", true);
+
+            javaMailSender.send(mailMessage);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
     }
-    
+
 }
